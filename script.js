7169bd920feac6172453cb364ee28779ff9c7741
Antanas Pužas <antanaspuzas@protonmail.com>
2022-06-09 18:14:14 +0300

    Implement basic calculator display

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
+
+const numberPad = ["=", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operatorPad = ["+", "-", "*", "/"];
+
+const keypadNumbers = document.querySelector("#keypad-numbers");
+const keypadOperators = document.querySelector("#keypad-operators");
+
+function populateElementButtons(padValues, element) {
+    padValues.forEach((value) => {
+        let button = document.createElement("button");
+        button.textContent = value;
+        element.appendChild(button);
+    })
+}
+
+populateElementButtons(numberPad, keypadNumbers);
+populateElementButtons(operatorPad, keypadOperators);
